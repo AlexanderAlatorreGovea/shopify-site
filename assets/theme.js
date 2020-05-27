3ae67c47d0eeaca36d487935fa72678cae42314c
Mitchell alatorre <alexander_alatorre1993@hotmail.com>
2020-05-26 21:01:02 -0700

    added sections

--- assets/theme.js
+++ assets/theme.js
@@ -7552,7 +7552,7 @@ theme.init = function() {
     theme.Helpers.setTouch();
   });
 };
-
+ 
 // Youtube API callback
 // eslint-disable-next-line no-unused-vars
 function onYouTubeIframeAPIReady() {
@@ -7560,5 +7560,183 @@ function onYouTubeIframeAPIReady() {
   theme.ProductVideo.loadVideos(theme.ProductVideo.hosts.youtube);
 }
 
+/* JUMBO ANIMATION */
+let jumboTL = gsap.timeline();
+
+jumboTL
+    .fromTo("#jumbo .transparent-color",
+        {
+            opacity: 0,
+            backgroundColor: "black"
+        },
+        {
+            opacity: .5,
+            duration: 2
+        }
+    ).fromTo(".logo",
+        {
+            x: -200,
+            opacity: 0
+        },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 1.5
+        }
+    ).fromTo(".menu",
+        {
+            x: 200,
+            opacity: 0
+        },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 1.3
+        },
+        '> -1.3s'
+    ).fromTo("#jumbo .titles h1",
+        {
+            x: 0,
+            opacity: 0
+        },
+        {
+            x: 100,
+            opacity: 1,
+            duration: 1
+        },
+        '> -.3s'
+    ).fromTo("#jumbo .titles h2",
+        {
+            x: 0,
+            opacity: 0
+        },
+        {
+            x: 135,
+            opacity: 1,
+            duration: 1
+        },
+        '> -.8s'
+    ).fromTo("#jumbo .state",
+        {
+            x: "100%"
+        },
+        {
+            x: 0,
+            duration: 1
+        }
+    ).fromTo("#jumbo .store-info",
+        {
+            y: "100%"
+        },
+        {
+            y: 0,
+            duration: 1
+        },
+        '> -1.8'
+    ).fromTo("#jumbo .store-info img",
+        {
+            y: 400,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            stagger: .2
+        },
+        '> -1.8'
+    );
+/* JUMBO TWO COLLECTION ANIMATION */
+
+const twoCollectionTL = gsap.timeline();
+
+twoCollectionTL
+    .fromTo('#two-collections .col-md-6',
+        {
+            y: 300,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1
+        }
+    ).fromTo('#two-collections .left-c span',
+        {
+            y: 300,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1
+        }
+    ).fromTo('#two-collections .right-c span',
+        {
+            y: 300,
+            opacity: 0
+        },
+        {
+            y: -100,
+            opacity: 1,
+            duration: 1
+        },
+        '> -.8'
+    );
+
+let homeController = new ScrollMagic.Controller();
+
+let twoCScene = new ScrollMagic.Scene({
+        triggerElement: "#two-collections",
+        triggerHook: .9,
+        reverse: false,
+        offset: 100,
+        duration: 0
+        //duration: document.querySelector("#two-collections").offsetHeight
+    })
+    .setTween(twoCollectionTL)
+    //.addIndicators()
+    .addTo(homeController)
+
+/* HEADER ANIMATION */
+const headerTL = gsap.timeline();
+
+headerTL
+    .fromTo(
+        ".header", 
+        {
+            backgroundColor: "rgba(0,0,0,0)"
+        },
+        {
+            backgroundColor: "rgba(0,0,0,1)",
+            duration: .4
+        }
+    ).fromTo(
+        ".header .logo a",
+        {
+            scale: 1
+        },
+        {
+            scale: .8,
+            duration: .4
+        },
+        "> .-4"
+    );
+
+let headerScene = new ScrollMagic.Scene({
+        triggerElement: "#jumbo",
+        triggerHook: 1,
+        reverse: true,
+        offset: (document.querySelector("#jumbo").offsetHeight) + 100,
+        duration: 0
+    })
+    .setTween(headerTL)
+    .addTo(homeController)
+    
+    
+
 $(theme.init);
+
+
+
 
